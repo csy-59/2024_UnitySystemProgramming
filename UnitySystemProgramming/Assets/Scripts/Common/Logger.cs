32ac1edcee898eb93c45e8f1c67fd0fa06807ed7
csy-59 <csy010509@naver.com>
2024-09-05 14:26:53 +0900

    [Feat] 로고 및 타이틀 처리

--- UnitySystemProgramming/Assets/Scripts/Common/Logger.cs
+++ UnitySystemProgramming/Assets/Scripts/Common/Logger.cs
@@ -7,17 +7,17 @@ public static class Logger
     [Conditional("DEV_VER")]
     public static void Log(string msg)
     {
-        UnityEngine.Debug.LogFormat("[{0}] [1]", System.DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss"), msg);
+        UnityEngine.Debug.LogFormat("[{0}] [{1}]", System.DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss"), msg);
     }
 
     [Conditional("DEV_VER")]
     public static void LogWarning(string msg)
     {
-        UnityEngine.Debug.LogWarningFormat("[{0}] [1]", System.DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss"), msg);
+        UnityEngine.Debug.LogWarningFormat("[{0}] [{1}]", System.DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss"), msg);
     }
 
     public static void LogError(string msg)
     {
-        UnityEngine.Debug.LogErrorFormat("[{0}] [1]", System.DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss"), msg);
+        UnityEngine.Debug.LogErrorFormat("[{0}] [{1}]", System.DateTime.Now.ToString("yyyy-mm-dd HH:mm:ss"), msg);
     }
 }
