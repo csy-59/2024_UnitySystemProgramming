1d1299ca7ab13404da16b0c40d8c1cb245f94681
csy-59 <csy010509@naver.com>
2024-09-09 15:48:56 +0900

    [Feat] 유저 데이터 시스템 구현

---
+++ UnitySystemProgramming/Assets/Scripts/Common/UserData/UserGoodsData.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class UserGoodsData : IUserData
+{
+    // º¸¼®
+    public long Gem { get; set; }
+    // °ñµå
+    public long Gold { get; set; }
+
+    public void SetDefaultData()
+    {
+        Logger.Log($"{GetType()}::SetlDefaultData");
+    }
+
+    public bool LoadData()
+    {
+        Logger.Log($"{GetType()}::LoadData");
+
+        bool result = false;
+
+        try
+        {
+            Gem = long.Parse(PlayerPrefs.GetString("Gem"));
+            Gold = long.Parse(PlayerPrefs.GetString("Gold"));
+            result = true;
+
+            Logger.Log($"Gem:{Gem}, Gold:{Gold}");
+        }
+        catch (System.Exception e)
+        {
+            Logger.Log("Load failed (" + e.Message + ")" );
+        }
+
+        return result;
+    }
+
+    public bool SaveData()
+    {
+        Logger.Log($"{GetType()}::SetlDefaultData");
+
+        bool result = false;
+
+        try
+        {
+            PlayerPrefs.SetString("Gem", Gem.ToString());
+            PlayerPrefs.SetString("Gold", Gold.ToString());
+            result = true;
+
+            Logger.Log($"Gem:{Gem}, Gold:{Gold}");
+        }
+        catch (System.Exception e)
+        {
+            Logger.Log("Load failed (" + e.Message + ")");
+        }
+
+        return result;
+    }
+}
